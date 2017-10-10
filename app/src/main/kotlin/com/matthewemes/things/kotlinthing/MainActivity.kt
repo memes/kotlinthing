887f1f6465a5a5e05120c1305a5d6c67c171e172
Matthew Emes <memes@matthewemes.com>
2017-10-10 15:43:52 -0700

    Minor changes  - use consistent version names in root build.gradle for kotlin and gradle plugin versions  - apply kotlin-android-extensions

--- app/src/main/kotlin/com/matthewemes/things/kotlinthing/MainActivity.kt
+++ app/src/main/kotlin/com/matthewemes/things/kotlinthing/MainActivity.kt
@@ -2,11 +2,14 @@ package com.matthewemes.things.kotlinthing
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        welcomeText.text = "Using Kotlin android extensions!"
     }
 }
