abc957340caabdc03d6fef2e75ea87a37ad92711
Matthew Emes <memes@matthewemes.com>
2017-10-11 22:04:26 -0700

    Semi-obligatory LED blinker to make sure things are working!

--- app/src/main/kotlin/com/matthewemes/things/kotlinthing/MainActivity.kt
+++ app/src/main/kotlin/com/matthewemes/things/kotlinthing/MainActivity.kt
@@ -2,21 +2,59 @@ package com.matthewemes.things.kotlinthing
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.PeripheralManagerService
 import timber.log.Timber
 
 class MainActivity : Activity() {
 
+    companion object {
+        val GPIO_PIN_NAME = "BCM6"
+        val BLINK_INTERVAL_MS = 1000L
+    }
+
+    private val handler = Handler()
+    private var gpio: Gpio? = null
+    private var blinker: Runnable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Timber.d("onCreate: enter, savedInstanceState = %s", savedInstanceState)
         super.onCreate(savedInstanceState)
-
-        val gpios = PeripheralManagerService().gpioList
-        if (gpios.isEmpty()) {
-            Timber.i("Didn't find any GPIO pins")
-        } else {
-            Timber.i("Found GPIO pins: %s", gpios.joinToString())
+        val service = PeripheralManagerService()
+        try {
+            gpio = service.openGpio(GPIO_PIN_NAME)
+            gpio?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+            if (blinker == null) {
+                blinker = Runnable {
+                    if (gpio != null) {
+                        try {
+                            gpio!!.value = !gpio!!.value
+                            handler.postDelayed(blinker, BLINK_INTERVAL_MS)
+                        } catch (t: Throwable) {
+                            Timber.e(t, "Caught an exception in blinker runnable")
+                        }
+                    }
+                }
+            }
+            handler.post(blinker)
+        } catch (t: Throwable) {
+            Timber.e(t, "Caught exception setting up GPIO")
         }
         Timber.d("onCreate: exit")
+    }
+
+    override fun onDestroy() {
+        Timber.d("onDestroy: enter")
+        super.onDestroy()
+        handler.removeCallbacks(blinker)
+        try {
+            gpio?.value = false
+            gpio?.close()
+            gpio = null
+        } catch (t: Throwable) {
+            Timber.e(t, "Caught an exception shutting off GPIO")
+        }
+        Timber.d("onDestroy: exit")
     }
 }
